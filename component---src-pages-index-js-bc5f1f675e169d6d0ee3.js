ec6f8ef657607e9b436aa308f7a8260f228c9837
Bryan Bailey <bcbailey84@gail.com>
2022-05-09 22:11:25 -0400

    Updates

---
+++ component---src-pages-index-js-bc5f1f675e169d6d0ee3.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_hello_world=self.webpackChunkgatsby_starter_hello_world||[]).push([[678],{558:function(e,t,r){r.r(t),r.d(t,{default:function(){return n}});var l=r(294);function n(){return l.createElement("div",null,"www.brizzle.dev")}}}]);
+//# sourceMappingURL=component---src-pages-index-js-bc5f1f675e169d6d0ee3.js.map
