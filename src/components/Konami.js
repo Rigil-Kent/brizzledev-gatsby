7eae86c34af175f277f1c9095d4b73b6751abb9a
Bryan Bailey <bcbailey84@gail.com>
2022-05-16 23:10:28 -0400

    konami code styling

--- src/components/Konami.js
+++ src/components/Konami.js
@@ -1,61 +1,88 @@
-import React from 'react'
-
-
-const dullard = ['ArrowDown', 'ArrowUp', 'ArrowLeft', 'ArrowLeft', 'a', 'ArrowRight', 'ArrowDown'];
-
-function isEqual(a, b) {
-    if ( a === b) return true;
-
-    if (a === null || b === null) return false;
-
-    if (a.length !== b.length) return false;
-
-    for (let i in a.length) {
-        if (a[i] !== b[i]) return false;
-    }
-
-    return true;
-}
-
-function code_check() {
-    document.addEventListener('keypress', event => {
-        
-        if (event.key === 's') {
-            var keystrokes = [];
-
-            console.log(typeof(keystrokes));
-            // arrow keys are only triggered by 'onkeydown'
-            // document.addEventListener('keydown', e => {
-            //     // prevent arrow keys from triggering scroll and s from repeating and messing up the keystrokes array
-            //     const validKeys = ["ArrowDown","ArrowLeft","ArrowRight", "ArrowUp", 'a'];
-
-            //     if (["ArrowDown","ArrowLeft","ArrowRight", "ArrowUp", 'a', 's'].indexOf(e.code) > -1) {
-            //         e.preventDefault();
-            //     }
-
-            //     console.log(e.key in validKeys);
-
-            //     if(validKeys.includes(e.key)) {
-            //         keystrokes.push(e.key);
-            //     }
-
-            //     if (isEqual(keystrokes, dullard)) {
-            //         console.log('woo');
-            //     } else {
-            //         keystrokes = [];
-            //     }
-
-            // });
-        }
-    });
-}
+import React, { useEffect } from 'react'
 
 
 export default function Konami() {
+    
+    function isEqual(a, b) {
+        // if ( a === b) return true;
+    
+        // if (a === null || b === null) return false;
+    
+        // if (a.length !== b.length) return false;
+    
+        // for (let i in a.length) {
+        //     if (a[i] !== b[i]) return false;
+        // }
+    
+        // return true;
+        return a.length === b. length && a.every((value, index) => value === b[index])
+    }
 
-    code_check();
+    useEffect(_ => {
+        const dullard = ['ArrowDown', 'ArrowUp', 'ArrowLeft', 'ArrowLeft', 'a', 'ArrowRight', 'ArrowDown']
+        let keystrokes = []
+        let pressed = {}
+
+        const handleKeystrokes = (event) => {
+            let active = false;
+            let konami_div = event.target.lastChild.firstChild
+            pressed[event.key] = true
+
+            if (event.key === '`') konami_div.classList.toggle('is-open')
+            document.body.style.overflowY = 'hidden'
+
+
+            if (pressed['`']) {
+
+                if (active) {
+                    delete pressed['`']
+                    active = false
+                    keystrokes.length = 
+                    konami_div.classList.remove('is-open')
+                    document.body.style.overflowY = 'scroll'
+                    return
+                }
+
+                active = true
+                
+                if (keystrokes.length > 6) keystrokes.length = 0
+
+                keystrokes.push(event.key)
+
+                if (isEqual(keystrokes, dullard)) {
+                    console.log('success')
+                    delete pressed['`']
+                    window.location.href = '/about'
+                }
+
+                keystrokes.forEach((_, index) => {
+                    if (!isEqual(keystrokes.slice(0, index + 1), dullard.slice(0, index + 1))) {
+                        // reset keystrokes on mismatch
+                        keystrokes.length = 0
+                    }
+                })
+            }
+
+        }
+
+        window.addEventListener('keydown', handleKeystrokes)
+
+        return _ => {
+            window.removeEventListener('keydown', handleKeystrokes)
+        }
+    }, [])
 
   return (
-    <div></div>
+    <div className='konami'>
+        <div className='keyentry'>
+            <img src='keyboard-key-down.svg' alt='keyboard key down' />
+            <img src='keyboard-key-up.svg' alt='keyboard key up' />
+            <img src='keyboard-key-left.svg' alt='keyboard key left' />
+            <img src='keyboard-key-left.svg' alt='keyboard key left' />
+            <img src='keyboard-key-a.svg' alt='keyboard key a' />
+            <img src='keyboard-key-right.svg' alt='keyboard key right' />
+            <img src='keyboard-key-down.svg' alt='keyboard key down' />
+        </div>
+    </div>
   )
 }
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,10 +3,12 @@ import Footer from './Footer';
 // import Navbar from './Navbar';
 import '../styles/global.scss';
 import SlideNav from './SlideNav';
+import Konami from './Konami';
 
 export default function Layout({ children }) {
   return (
     <div className='layout'>
+        <Konami />
         <SlideNav />
 
         <div className='page-content'>
