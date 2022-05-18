a64245f173825b10c795c60dccfe7d43b2724ee4
Bryan Bailey <bcbailey84@gail.com>
2022-05-17 23:18:28 -0400

    fixed graphql query

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -16,20 +16,14 @@ export default function Bio() {
     }
     `);
 
-  const getData = (data) => {
-    if (data.contentfulSiteInfo.bio.bio === '') return <LoremIpsum p={2} />
-
-    return data.contentfulSiteInfo.bio.bio
-  }
-
 
   return (
     <section id='about' className='bio'>
         <img className='bio-image' src='john-portfolio.png' alt='john portfolio sillhouette shot' />
-        <h1 className='bio-title'>Bryan Bailey</h1>
-        <h2 className='bio-subtitle'>Fullstack Developer</h2>
+        <h1 className='bio-title'>{data.contentfulSiteInfo.mainHeading}</h1>
+        <h2 className='bio-subtitle'>{data.contentfulSiteInfo.tagLine}</h2>
         <div className='bio-text'>
-            {getData(data)}
+            { data.contentfulSiteInfo.bio.bio }
         </div>
     </section>
   )
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,7 +1,19 @@
+import { graphql, useStaticQuery } from 'gatsby';
 import React from 'react';
 import LoremIpsum from 'react-lorem-ipsum';
 
 export default function Intro() {
+
+  const data = useStaticQuery(graphql`
+    query IntroInfo {
+      contentfulSiteInfo {
+        bio {
+          bio
+        }
+      }
+    }
+    `);
+
   return (
     <section className='intro'>
         <div className='intro-left'>
@@ -9,7 +21,7 @@ export default function Intro() {
         </div>
         <div className='intro-middle'>
             <h2>Modern Day Wizardry</h2>
-            <LoremIpsum p={1} />
+            <p>{ data.contentfulSiteInfo.bio.bio }</p>
         </div>
         <div className='intro-right'>
         <img src='welcome-02.png' alt='two desktops and a laptop on a wooden table' />
